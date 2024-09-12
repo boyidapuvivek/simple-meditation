92028078f75160960b0ab41d6f0f2f3331a50ebc
vivekboyidapu <102375767+boyidapuvivek@users.noreply.github.com>
2024-09-12 11:14:08 +0530

    12-09-24

---
+++ CaptureRide/app/index.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import SplashScreen from './screens/SplashScreen'
+import HomeScreen from './screens/HomeScreen'
+import React from 'react'
+
+
+const App = () => {
+  return (
+   <>
+   
+   <HomeScreen />
+   <SplashScreen />
+   
+   </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ CaptureRide/app/screens/SplashScreen.jsx
@@ -0,0 +1,13 @@
+import { View,Text,StyleSheet} from 'react-native';
+
+
+function SplashScreen(){
+    <View>
+        <Text>this is splash screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+})
+
+export default SplashScreen;
